e792b9955c51fc4f6e88de2fb369b85d7ce00a99
Capmare <dnCapmare@gmail.com>
2019-07-29 22:53:09 +0200

    gud

---
+++ sf.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git config --global user.email "dnCapmare@gmail.com"
+git config --global user.name "Capmare"
+
+git init 
+git add .
+git commit -m 'gud'
+git remote add origin https://github.com/Capmare/Files.git
+git push -u origin master
+
